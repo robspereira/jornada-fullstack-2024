3522cb089753f48b84e519ef9c6becf92d3eb939
Roberto Lucas <robertolucas335@hotmail.com>
2024-05-29 17:04:54 -0300

    Implementação do dbcontext e mapeamento das entidades

---
+++ Fina.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using Fina.Api.Data.Mappings;
+using Fina.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fina.Api.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetCallingAssembly());
+
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+}
